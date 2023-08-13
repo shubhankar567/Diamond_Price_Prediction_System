65c1bdc1d85292543e2a16a71d09ae04853bf74a
shubhankar567 <shubhankar5848@gmail.com>
2023-08-13 08:10:32 +0530

    Setup.py commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="abvc",
-    author_email="abddjbx@gmail.com",
+    author="Shubhankar Chaturvedi",
+    author_email="shubhankar5848@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
